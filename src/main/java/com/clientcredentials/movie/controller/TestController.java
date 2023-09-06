a03f8e03e5905e6c9c4531de7e21db58d36ef73f
daniel.jimenezm <daniel.jose.45@hotmail.com>
2023-09-06 08:43:52 -0500

    adding support for environment variables

--- src/main/java/com/clientcredentials/movie/controller/TestController.java
+++ src/main/java/com/clientcredentials/movie/controller/TestController.java
@@ -4,12 +4,24 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@PreAuthorize("hasRole('admin_client')")
 @RestController("/")
 public class TestController {
 
     @GetMapping
+    @PreAuthorize("hasRole('admin_client') or hasRole('user_client')")
     public String testEndpoint(){
         return "test-endpoint";
+    }
+
+    @GetMapping("admin")
+    @PreAuthorize("hasRole('admin_client')")
+    public String helloAdmin(){
+        return "test-endpoint-admin";
+    }
+
+    @GetMapping("user")
+    @PreAuthorize("hasRole('user_client')")
+    public String helloUser(){
+        return "test-endpoint-user";
     }
 }
